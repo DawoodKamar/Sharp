f09992c3f38ed9054c9399b170310d471644bbeb
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2023-08-24 23:44:20 -0400

    adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import Image from 'next/image'
+import RootLayout from "./layout";
+
 
 export default function Home() {
   return (<>
-    <Header/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Sharp project</h1>
     </main>
-    <Footer/>
     </>
   )
 }
