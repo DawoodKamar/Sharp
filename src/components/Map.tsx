271dd9f06aabb1943501a1e628cdf03b1be26996
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2023-09-25 23:59:29 -0400

    attempting to fix google maps bugs

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,15 @@
 "use client"
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api';
 
 
 
 function Map() {
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+
+  useEffect(() => {
+      setIsMapLoaded(true);
+  }, []);
     const containerStyle = {
         width: '800px',
         height: '400px'
@@ -16,8 +21,8 @@ function Map() {
       };
     const coordinates = [
         { lat: 43.3795, lng: -80.7114 }, // New Hamburg
-        { lat: 43.6832, lng: -80.4304 }, // Elora
-        { lat: 43.9193, lng: -80.0944 }, // Orangeville
+        { lat: 43.6097, lng: -80.4822 }, // Ariss
+        // { lat: 43.9193, lng: -80.0944 }, // Orangeville
         { lat: 43.7942, lng: -79.5268 }, // Vaughan
         { lat: 43.6205, lng: -79.5132 }, // Etobicoke
         { lat: 43.4675, lng: -79.6877 }, // Oakville
@@ -26,7 +31,7 @@ function Map() {
       ];
   return (
     <LoadScript
-      googleMapsApiKey="AIzaSyDbga1HSUALfqImumyk6mgf-RKEAdGPd0c"
+    googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
